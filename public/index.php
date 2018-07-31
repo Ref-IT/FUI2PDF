53bae3b3116623315c89d05e573216c4355fb07f
lu_kors+github@posteo.de <lu_kors+github@posteo.de>
2018-07-31 16:10:51 +0200

    new build up

---
+++ public/index.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @author michael
+ */
+//framework -----------------------------
+include "../lib/inc.all.php";
+
+// routing ------------------------------
+$router = new Router();
+$routeInfo = $router->route();
+
+//ip and apikey
+if($routeInfo['controller'] != 'error' && (!isset($routeInfo['allowall']) || !$routeInfo['allowall'] )){
+	// check API KEY
+	if ($_SERVER['REQUEST_METHOD'] == 'GET' && (!isset($_GET['APIKEY'])||$_GET['APIKEY']!=API_KEY)){
+		$routeInfo['action'] = '403';
+		$routeInfo['controller'] = 'error';
+		$routeInfo['method'] = 'POST';
+	} elseif($_SERVER['REQUEST_METHOD'] == 'POST' && (!isset($_POST['APIKEY'])||$_POST['APIKEY']!=API_KEY)) {
+		$routeInfo['action'] = '403';
+		$routeInfo['controller'] = 'error';
+		$routeInfo['method'] = 'POST';
+	}
+	// ip check
+	if ($_SERVER['REMOTE_ADDR'] && is_array($_SERVER['REMOTE_ADDR']) && !in_array($_SERVER['REMOTE_ADDR'], ALLOWED_IPS)){
+		$routeInfo['action'] = '403';
+		$routeInfo['controller'] = 'error';
+		$routeInfo['method'] = 'POST';
+	}
+}
+// auth ---------------------------------
+if (isset($routeInfo['auth']) && ($routeInfo['auth'] == 'Basic' || $routeInfo['auth'] == 'basic')){
+	define('AUTH_HANLER', 'AuthBasicHandler');
+	AuthBasicHandler::getInstance()->requireAuth();
+	AuthBasicHandler::getInstance()->requireGroup('basic');
+	if (!isset($routeInfo['groups'])){
+		$routeInfo['action'] = '403';
+		$routeInfo['controller'] = 'error';
+		$routeInfo['method'] = 'POST';
+	} elseif (isset($routeInfo['groups']) && !AuthBasicHandler::getInstance()->hasGroup($routeInfo['groups'])){
+		$routeInfo['action'] = '403';
+		$routeInfo['controller'] = 'error';
+		$routeInfo['method'] = 'POST';
+	}
+} else {
+	define('AUTH_HANLER', NULL);
+}
+
+// handle route -------------------------
+switch ($routeInfo['controller']){
+    case "old":
+        include SYSBASE.'/old/index.php';
+        break;
+    case "pdfbuilder":
+    	$builder = new TexBuilder();
+    	if (!AuthBasicHandler::getInstance()->hasGroup('pdfbuilder')
+    		|| !isset($_POST['action']) 
+    		|| !$builder->builderExist($_POST['action'])) {
+    		$routeInfo['action'] = '403';
+    		$routeInfo['controller'] = 'error';
+    		$routeInfo['method'] = 'POST';
+    		$errorHdl = new ErrorHandler($routeInfo);
+    		$errorHdl->render();
+    	} elseif(!$builder->validate($_POST['action'])
+    		|| !$builder->build()
+    		|| $builder->getError()) {
+    		JsonController::print_json([
+    			'success' => false,
+    			'controller' => $routeInfo['controller'],
+    			'error' => $builder->getError(),
+    		], true);
+    	} else {
+    		JsonController::print_json([
+    			'success' => true,
+    			'controller' => $routeInfo['controller'],
+    			'action' => $_POST['action'],
+    			'data' => $builder->getBase64(),
+    		], true);
+    	}
+        break;
+    case 'error':
+    default:
+        $errorHdl = new ErrorHandler($routeInfo);
+        $errorHdl->render();
+        break;
+}
+
+
