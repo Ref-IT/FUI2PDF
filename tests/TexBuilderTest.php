4e74d422454df20745305500503ff50607fe7ccc
Lukas Staab <lukas.staab@tu-ilmenau.de>
2019-02-24 16:02:19 +0100

    minor fix
    Signed-off-by: lu_kors <lu_kors+github@posteo.de>

---
+++ tests/TexBuilderTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konsul
+ * Date: 01.08.18
+ * Time: 18:12
+ */
+
+declare(strict_types=1);
+
+require_once "../vendor/autoload.php";
+
+class TexBuilderTest
+	extends PHPUnit_Framework_TestCase{
+	
+	public function testBuilderExist(){
+	
+	}
+	
+	public function testGetBase64(){
+	
+	}
+	
+	public function testGetBinary(){
+	
+	}
+	
+	public function testBuild(){
+	
+	}
+	
+	public function testGetError(){
+	
+	}
+	
+	public function testGetValidated(){
+	
+	}
+	
+	public function testSetData(){
+	
+	}
+	
+	public function testValidate(){
+	
+	}
+	
+	public function test__construct(){
+	
+	}
+}
